c83c4bc41a6c20040c9c63cc3b192af528a2e6ce
geryy <g@geryy.io>
2023-10-28 19:27:37 +0200

    navigation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'primary': "#372922",
+			"beige": "#F7ECE4",
+			"white": "#fff",
+			"highlight": "#DEAA82"
+		},
+		fontFamily: {
+			sans: ['Source Sans Pro', 'sans-serif'],
+			serif: ['Playfair Display Variable', 'serif'],
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				xl: '4rem',
+			},
+		},
 	},
 	plugins: [],
 }
