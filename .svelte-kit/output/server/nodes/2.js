771a1b25d19142bde5aa208fdf802e98170e6da3
geryy <g@geryy.io>
2024-01-13 01:09:30 +0100

    check for dev server

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.EJHuGVOv.js","_app/immutable/chunks/scheduler.RVCy_kJM.js","_app/immutable/chunks/index.JI7moINP.js","_app/immutable/chunks/OpenHoures.oWExQyHi.js"];
-export const stylesheets = ["_app/immutable/assets/2.Ph_99H91.css"];
-export const fonts = [];
