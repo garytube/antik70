d9fb3700f0a93af1f247eaa768d5152e21699690
Gerald Scholz <g@geryy.io>
2024-07-06 11:18:47 +0200

    FERIEN

--- src/lib/components/OpenHoures.svelte
+++ src/lib/components/OpenHoures.svelte
@@ -11,3 +11,7 @@
 	<span class="font-bold block">{SITE.DE.OPEN_HOURS_3}</span>
 	{SITE.DE.OPEN_HOURS_4}
 </p>
+
+<div class="row-start-2 w- block bg-redish-LIGHT rounded-md p-4 mt-6 text-white text-center">
+	Betriebsferien vom <br /> Montag 15.7.24 bis Samstag 3.8.24 <br /> <span class="font-bold">Mailanfragen werden bearbeitet!</span>
+</div>
